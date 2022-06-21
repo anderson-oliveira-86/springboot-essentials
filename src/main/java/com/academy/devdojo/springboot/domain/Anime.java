2fabe5d3985e3ce3e88922f10741afb92e27e838
Anderson <anderson.java.dev@gmail.com>
2022-06-21 18:53:28 -0300

    13 - Spring Data JPA pt01 and docker-compose

--- src/main/java/com/academy/devdojo/springboot/domain/Anime.java
+++ src/main/java/com/academy/devdojo/springboot/domain/Anime.java
@@ -2,12 +2,21 @@ package com.academy.devdojo.springboot.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
+@Entity
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
